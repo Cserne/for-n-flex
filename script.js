b9854e7a187a45299a8a22f6e8c4d922d9d232fc
Cserne <angeli.cserne@gmail.com>
2021-11-17 10:13:47 +0100

    Changes added.

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
-function loadEvent() {
+const generateArray = function(amount) {
+    let returnArray = [];
 
+    /*
+    Ez a 9.sorban azzal egyenlő, mintha azt mondtuk volna, hogy:
+    let amountConverted = parseInt(amount);
+    amount = amountConverted;
+    */
+    amount = parseInt(amount);
+    //console.log(amount);
+
+    if (!Number.isNaN(amount)) {
+        
+        //console.log("Az amount egy szám");
+        for (let i = 0; i < amount; i++) {
+            returnArray.push(`${i}`);
+        }
+    }
+    else {
+        //console.log("Az amount nem egy szám");
+        returnArray.push("Error");
+    }
+
+
+    return returnArray;
+}
+
+const loadEvent = function() {
+    console.log("Az oldal betöltődött.");
+
+    /*
+    console.log(generateArray("100"));
+    console.log(generateArray(15));
+    console.log(generateArray("kiskutya"));
+    console.log(generateArray([2]));
+    */
+   const root = document.getElementById("root");
+   const list = generateArray(200);
+
+   if (list[0] !== "Error") {
+       for (const item of list) {
+           root.insertAdjacentHTML("beforeend", `<div>${item}</div>`);
+       }
+   }
 }
 
 window.addEventListener("load", loadEvent);
+
+/*
+window.addEventListener("load", function() {
+    Ez is teljesen jó, ugyanaz kb., mint a fönti.
+});
+
+window.addEventListener ("load", () => {
+    arrow, ez is elfogadott
+});
+*/
